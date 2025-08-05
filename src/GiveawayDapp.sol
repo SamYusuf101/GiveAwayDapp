44ec4335f3a0e43a026fbfd522d525cb7bf3f732
samyusuf101 <samyusuf101@gmail.com>
2025-08-05 14:42:39 +0100

    created error message

--- src/GiveawayDapp.sol
+++ src/GiveawayDapp.sol
@@ -1,8 +1,6 @@
 //SPDX-License-Identifier : MIT
 pragma solidity 0.8.19;
 
-
-
 /**
  * @title A GiveawaDapp contract
  * @author Sam
@@ -10,20 +8,27 @@ pragma solidity 0.8.19;
  * @dev Implements Chainlink VRFv2.5
  */
 contract GiveawayDapp {
+    /*ERROS*/
+    error GiveAway__NotEnoughEth();
     uint256 private immutable i_entranceFee;
-    constructor(uint256 entranceFee){
-    i_entranceFee = entranceFee;
-    }
-    function joinGiveAway () public payable{
+    address payable[] public s_people;
 
-    }
-
-    function selectWinner () public {
-
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
     }
 
-    function getEntranceFee () public view returns (uint256) {
+    function joinGiveAway() public payable {
+        //  require(msg.value >= i_entranceFee,  NotEnoughEth());
+        if (msg.value < i_entranceFee) {
+            revert GiveAway__NotEnoughEth();
+        }
+
+        s_people.push(payable(msg.sender));
+    }
+
+    function selectWinner() public {}
+
+    function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
-
 }
