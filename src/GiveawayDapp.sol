1937c3b8a2fe4400a973db1fef3903008f9e83ef
samyusuf101 <samyusuf101@gmail.com>
2025-08-08 14:47:31 +0100

    Added constructor to initialize entrance fee, interval, and last timestamp

--- src/GiveawayDapp.sol
+++ src/GiveawayDapp.sol
@@ -1,5 +1,6 @@
 //SPDX-License-Identifier : MIT
 pragma solidity 0.8.19;
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 
 /**
  * @title A GiveawaDapp contract
@@ -7,14 +8,24 @@ pragma solidity 0.8.19;
  * @notice This contract is for creating a sample giveawayDapp
  * @dev Implements Chainlink VRFv2.5
  */
-contract GiveawayDapp {
+contract GiveawayDapp is VRFConsumerBaseV2Plus {
     /*ERROS*/
     error GiveAway__NotEnoughEth();
     uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    uint256 private s_lastTimeStamp;
     address payable[] public s_people;
 
-    constructor(uint256 entranceFee) {
+    event GiveawayEntered(address indexed number);
+
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
     function joinGiveAway() public payable {
@@ -24,11 +35,35 @@ contract GiveawayDapp {
         }
 
         s_people.push(payable(msg.sender));
+
+        emit GiveawayEntered(msg.sender);
     }
 
-    function selectWinner() public {}
+    function selectWinner() external {
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: s_keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
+    }
 
-    function getEntranceFee() public view returns (uint256) {
+    function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal override {}
 }
